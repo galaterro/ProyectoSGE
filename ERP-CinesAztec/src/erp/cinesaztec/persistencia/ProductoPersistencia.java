ec9fdb5fdf221f9e185429d66511cd8624f32d04
galaterro <adrix1992@gmail.com>
2017-01-25 16:09:07 +0100

    persistencia producto

---
+++ ERP-CinesAztec/src/erp/cinesaztec/persistencia/ProductoPersistencia.java
@@ -0,0 +1,51 @@
+package erp.cinesaztec.persistencia;
+
+import erp.cinesaztec.modelo.Producto;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Created by juanxxiii on 25/01/2017.
+ */
+public class ProductoPersistencia {
+
+    GestorBBDD gbd = new GestorBBDD();
+    private Statement st = null;//sentencia a ejecutar
+    private ResultSet rs = null;//resultado
+    private PreparedStatement ps = null;
+    private Connection c;
+    Producto producto;
+    ArrayList<Producto> alProducto = new ArrayList();
+
+    public void ingresarProducto(Producto producto) throws SQLException, ClassNotFoundException {
+        int filasAfectadas;
+
+        String sql = "insert into producto values(" + producto.getId_producto() + ",'" + producto.getDesc_producto() + "','" + producto.getPrecio_producto() + "','" + producto.getPrecio_venta()+ "','" + producto.getNombre_producto() + "')";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        filasAfectadas = st.executeUpdate(sql);
+        System.out.println("filas afectadas: " + filasAfectadas);
+        gbd.cerrarConexionBBDD();
+    }
+
+    public ArrayList listarProducto() throws SQLException, ClassNotFoundException {
+        gbd.conectarBBDD();
+
+        String sql = "select * from producto";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        rs = st.executeQuery(sql);
+        System.out.println("Las Salas son: ");
+        while (rs.next()) {
+            producto = new Producto(rs.getInt(1), rs.getString(2), (Float) rs.getBigDecimal(3).floatValue(), rs.getBigDecimal(4).floatValue(), rs.getString(5));
+            alProducto.add(producto);
+        }
+        gbd.cerrarConexionBBDD();
+        return alProducto;
+
+    }
+
+}
