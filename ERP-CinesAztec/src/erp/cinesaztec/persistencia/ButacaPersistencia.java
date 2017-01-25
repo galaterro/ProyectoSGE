5737e4e8b8288724ac4dd769ba4501d7c7b8c72a
Kevin Castillo Escudero <kcastilloescudero@gmail.com>
2017-01-25 16:01:24 +0100

    Persistencia de Butaca y Cliente.

---
+++ ERP-CinesAztec/src/erp/cinesaztec/persistencia/ButacaPersistencia.java
@@ -0,0 +1,55 @@
+package erp.cinesaztec.persistencia;
+
+import erp.cinesaztec.modelo.Butaca;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author juanxxiiiTarde
+ */
+public class ButacaPersistencia {
+    
+    GestorBBDD gbd = new GestorBBDD();
+    private Statement st = null;//sentencia a ejecutar
+    private ResultSet rs = null;//resultado
+    private PreparedStatement ps = null;
+    private Connection c;
+    private Butaca butaca;
+    ArrayList<Butaca> alButaca = new ArrayList();
+    
+    public void ingresarButaca(Butaca butaca) throws SQLException, ClassNotFoundException {
+        gbd.conectarBBDD();
+
+        int filasAfectadas;
+
+        String sql = "insert into butaca values(" + butaca.getId_butaca() + ", " + butaca.getNumero_fila() + ", " + butaca.getId_sala() + ")";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        filasAfectadas = st.executeUpdate(sql);
+        System.out.println("Filas afectadas: " + filasAfectadas);
+        gbd.cerrarConexionBBDD();
+    }
+
+    public ArrayList listarButacas() throws ClassNotFoundException, SQLException {
+        gbd.conectarBBDD();
+
+        String sql = "select * from  butaca";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        rs = st.executeQuery(sql);
+        System.out.println("Las butacas son: ");
+        while (rs.next()) {
+            butaca = new Butaca(rs.getInt(1), rs.getInt(2), rs.getInt(3));
+            alButaca.add(butaca);
+        }
+        gbd.cerrarConexionBBDD();
+        return alButaca;
+    }
+}
---
+++ ERP-CinesAztec/src/erp/cinesaztec/persistencia/ClientePersistencia.java
@@ -0,0 +1,55 @@
+package erp.cinesaztec.persistencia;
+
+import erp.cinesaztec.modelo.Butaca;
+import erp.cinesaztec.modelo.Cliente;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author juanxxiiiTarde
+ */
+public class ClientePersistencia {
+    GestorBBDD gbd = new GestorBBDD();
+    private Statement st = null;//sentencia a ejecutar
+    private ResultSet rs = null;//resultado
+    private PreparedStatement ps = null;
+    private Connection c;
+    private Cliente cliente;
+    ArrayList<Cliente> alCliente = new ArrayList();
+    
+    public void ingresarCliente(Cliente cliente) throws SQLException, ClassNotFoundException {
+        gbd.conectarBBDD();
+
+        int filasAfectadas;
+
+        String sql = "insert into cliente values(" + cliente.getId_cliente()+ ", '" + cliente.getDni_cliente()+ "', '" + cliente.getNombre_cliente()+ "', '" + cliente.getApellidos_cliente() + "', " + cliente.getTelefono_cliente() + ", " + cliente.getCodpos_cliente() + ", " + cliente.getPuntos() + ", '" + cliente.getUsuario_cliente() + "', '" + cliente.getContraseña_cliente() + "')";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        filasAfectadas = st.executeUpdate(sql);
+        System.out.println("Filas afectadas: " + filasAfectadas);
+        gbd.cerrarConexionBBDD();
+    }
+
+    public ArrayList listarClientes() throws ClassNotFoundException, SQLException {
+        gbd.conectarBBDD();
+
+        String sql = "select * from cliente";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        rs = st.executeQuery(sql);
+        System.out.println("Los clientes son: ");
+        while (rs.next()) {
+            cliente = new Cliente(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getInt(6), rs.getInt(7), rs.getString(8), rs.getString(9));
+            alCliente.add(cliente);
+        }
+        gbd.cerrarConexionBBDD();
+        return alCliente;
+    }
+}
