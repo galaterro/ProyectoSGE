723df7343ff4240f95f6c824eeab28e4eb4330b2
bote212 <sergio28922@gmail.com>
2017-01-25 16:17:07 +0100

    Reserva y sesion persistencia

---
+++ ERP-CinesAztec/src/erp/cinesaztec/persistencia/ReservaPersistencia.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erp.cinesaztec.persistencia;
+
+import erp.cinesaztec.modelo.Reserva;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author juanxxiii
+ */
+public class ReservaPersistencia {
+    GestorBBDD gbd = new GestorBBDD();
+    private Statement st = null;//sentencia a ejecutar
+    private ResultSet rs = null;//resultado
+    private PreparedStatement ps = null;
+    private Connection c;
+    Reserva reserva;
+    ArrayList<Reserva> alReserva = new ArrayList();
+
+    public void ingresarReserva(Reserva reserva) throws SQLException, ClassNotFoundException {
+        gbd.conectarBBDD();
+
+        int filasAfectadas;
+
+        String sql = "insert into sala values(" + reserva.getId_reserva()+ ",'" + reserva.getId_pelicula()+ "','" + reserva.getId_sesion() + "','" + reserva.getId_butaca() + "','" + reserva.getId_cliente() + "','" + reserva.getId_sala() + ")";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        filasAfectadas = st.executeUpdate(sql);
+        System.out.println("filas afectadas: " + filasAfectadas);
+        gbd.cerrarConexionBBDD();
+    }
+
+    public ArrayList listarSalas() throws SQLException, ClassNotFoundException {
+        gbd.conectarBBDD();
+
+        String sql = "select * from sala";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        rs = st.executeQuery(sql);
+        System.out.println("LasSalas son: ");
+        while (rs.next()) {
+            reserva = new Reserva(rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getInt(5), rs.getInt(6));
+            alReserva.add(reserva);
+        }
+        gbd.cerrarConexionBBDD();
+        return alReserva;
+
+    }
+
+}
---
+++ ERP-CinesAztec/src/erp/cinesaztec/persistencia/SesionPersistencia.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erp.cinesaztec.persistencia;
+
+
+import erp.cinesaztec.modelo.Sesion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author juanxxiii
+ */
+public class SesionPersistencia {
+    
+    GestorBBDD gbd = new GestorBBDD();
+    private Statement st = null;//sentencia a ejecutar
+    private ResultSet rs = null;//resultado
+    private PreparedStatement ps = null;
+    private Connection c;
+    Sesion sesion;
+    ArrayList<Sesion> alSesion = new ArrayList();
+
+    
+     public void ingresarSesion(Sesion sesion) throws SQLException, ClassNotFoundException {
+        gbd.conectarBBDD();
+
+        int filasAfectadas;
+
+        String sql = "insert into sesion values(" + sesion.getId_sesion()+ ",'" + sesion.getHora_sesion() + "','" + sesion.getId_pelicula()+ "','" + sesion.getId_sala() + ")";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        filasAfectadas = st.executeUpdate(sql);
+        System.out.println("filas afectadas: " + filasAfectadas);
+        gbd.cerrarConexionBBDD();
+    }
+
+    public ArrayList listarSesion() throws ClassNotFoundException, SQLException {
+        gbd.conectarBBDD();
+
+        String sql = "select * from cine";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        rs = st.executeQuery(sql);
+        System.out.println("Los cines son: ");
+        while (rs.next()) {
+            sesion = new Sesion(rs.getInt(1), rs.getDate(2), rs.getInt(3), rs.getInt(4));
+            alSesion.add(sesion);
+        }
+        gbd.cerrarConexionBBDD();
+        return alSesion;
+    }
+}
