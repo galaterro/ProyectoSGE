dbba6ec3a2bcf11fad505129ce320e81eeb08250
victAll <victormanuel.allende@gmail.com>
2017-01-27 17:15:40 +0100

    ProveedorPersistencia
    probado good!

--- ERP-CinesAztec/src/erp/cinesaztec/Controlador/Ejecutador.java
+++ ERP-CinesAztec/src/erp/cinesaztec/Controlador/Ejecutador.java
@@ -1,8 +1,10 @@
 package erp.cinesaztec.Controlador;
 
 import erp.cinesaztec.modelo.Entrada;
+import erp.cinesaztec.modelo.Proveedor;
 import erp.cinesaztec.modelo.Sala;
 import erp.cinesaztec.persistencia.EntradaPersistencia;
+import erp.cinesaztec.persistencia.ProveedorPersistencia;
 import erp.cinesaztec.persistencia.SalaPersistencia;
 import java.math.BigDecimal;
 
@@ -16,18 +18,23 @@ public class Ejecutador {
     public static void main(String[] args) {
         SalaPersistencia sp = new SalaPersistencia();
         EntradaPersistencia ep = new EntradaPersistencia();
-        ArrayList<Entrada> alE = new ArrayList();
-        Entrada entra ;
-        
+        ArrayList<Proveedor> alP = new ArrayList();
+        Proveedor p;
+        ProveedorPersistencia pp = new ProveedorPersistencia();
         try {
-            //sp.ingresarSala(new Sala(1,"sala porno", 25,1));
-            ep.ingresarEntrada(new Entrada(7,  9.50));
-           
-             alE = ep.listarEntradas();
+            
+                
+             p = new Proveedor(3, "12344", "Tipo", "Puesto", 5487, "Madrid", 28500);
+             pp.ingresarProveedor(p);
              
-             for (Entrada e : alE) {
-                 System.out.println(e.getId_entrada()+" " + e.getPrecio_entrada());
+             alP = pp.listarProveedores();
+             
+             for (Proveedor pro : alP) {
+                
+                 System.out.println(pro.getId_proveedor()+" "+ pro.getNombre_proveedor()+" "+ pro.getApellidos_proveedor());
             }
+             
+             
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
---
+++ ERP-CinesAztec/src/erp/cinesaztec/persistencia/ProveedorPersistencia.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package erp.cinesaztec.persistencia;
+
+import erp.cinesaztec.modelo.Entrada;
+import erp.cinesaztec.modelo.Proveedor;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author allen
+ */
+public class ProveedorPersistencia {
+    GestorBBDD gbd = new GestorBBDD();
+    private Statement st = null;//sentencia a ejecutar
+    private ResultSet rs = null;//resultado
+    private PreparedStatement ps = null;
+    private Connection c;
+    Proveedor proveedor;
+    ArrayList<Proveedor> alProveedor = new ArrayList();
+    
+    
+    public void ingresarProveedor(Proveedor proveedor) throws SQLException, ClassNotFoundException {
+        gbd.conectarBBDD();
+
+        int filasAfectadas;
+
+        String sql = "insert into proveedor values("+proveedor.getId_proveedor()+ ",'" + proveedor.getCif_proveedor() + "','" + proveedor.getNombre_proveedor() +"','"+proveedor.getApellidos_proveedor()+"',"+proveedor.getTelefono_proveedor()+",'"+proveedor.getPob_proveedor()+"',"+proveedor.getCodPos_proveedor()+")";
+        
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+
+        filasAfectadas = st.executeUpdate(sql);
+        System.out.println("filas afectadas: " + filasAfectadas);
+        gbd.cerrarConexionBBDD();
+    }
+    
+    public ArrayList listarProveedores() throws SQLException, ClassNotFoundException {
+        gbd.conectarBBDD();
+
+        String sql = "select * from proveedor";
+        c = gbd.conectarBBDD();
+        st = c.createStatement();
+        rs = st.executeQuery(sql);
+        while (rs.next()) {
+            proveedor = new Proveedor(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getString(6), rs.getInt(7));
+            alProveedor.add(proveedor);
+           
+        }
+        gbd.cerrarConexionBBDD();
+        return alProveedor;
+
+    }
+}
